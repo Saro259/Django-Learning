aeae66ff9fae1ba7ec8e45abedcda2c8fff2b3ca
Saro259 <73172999+Saro259@users.noreply.github.com>
2023-10-15 00:01:52 +0530

    Delete social_media_app_project directory

--- social_media_app_project/authentication/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.contrib.auth import get_user_model
-
-# Create your models here.
-User = get_user_model() # Using in-built user model to get the user model from the particula function
